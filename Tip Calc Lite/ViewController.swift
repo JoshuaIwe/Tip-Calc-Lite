c500f9b10ebb9ec98691502d165ef97ad8097f82
Joshua Iwe <joshuaiwe@utexas.edu>
2019-01-23 15:22:43 -0600

    Initial Commit

---
+++ Tip Calc Lite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calc Lite
+//
+//  Created by Joshua Iwe on 1/23/19.
+//  Copyright © 2019 Joshua Iwe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tip Calc LiteTests/Tip_Calc_LiteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tip_Calc_LiteTests.swift
+//  Tip Calc LiteTests
+//
+//  Created by Joshua Iwe on 1/23/19.
+//  Copyright © 2019 Joshua Iwe. All rights reserved.
+//
+
+import XCTest
+@testable import Tip_Calc_Lite
+
+class Tip_Calc_LiteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
